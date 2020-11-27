6cdd435861d535a609d67156d3e21d9e9be629a4
Severin Langberg <langberg91@gmail.com>
2020-11-27 12:43:13 +0100

    added overlapping profiles filter

--- src/graph_learning/profile_synthesis.py
+++ src/graph_learning/profile_synthesis.py
@@ -64,6 +64,25 @@ def cross_correlation(i, N_i, M):
     return tau_i_star, N_i_star, C_i_star
 
 
+def overlaping_neighbours(M, i):
+    
+    # Exclude profile i.
+    candidates = np.concatenate([np.arange(i), np.arange(i + 1, M.shape[0])])
+    M_j = M[candidates]
+
+    t_start_i = np.argmax(M[i] != 0)
+    t_end_i = np.argmax(np.cumsum(M[i]))
+
+    # Start and end times for candidates.
+    t_start_j = np.argmax(M_j != 0, axis=1)
+    t_end_j = np.argmax(np.cumsum(M_j, axis=1), axis=1)
+
+    eligeble = np.logical_and(t_start_j <= t_start_i, t_end_i <= t_end_j)
+
+    # Candidates starting no later than m_i and ending no sooner than m_i.
+    return M_j[eligeble]
+
+
 def align(M_j, taus):
     
     M_j_aligned = []
